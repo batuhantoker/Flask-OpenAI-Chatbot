059b562833a3eb7a61a7cf624c0cf35dd0ce9a4c
JayJhaveri1906 <jjhaveri1906@gmail.com>
2024-10-22 12:03:36 -0700

    Hosted MongoDb to Server

--- data_classes/mongo_setup.py
+++ data_classes/mongo_setup.py
@@ -1,5 +1,12 @@
 import mongoengine
-
+import json
 
 def global_init():
-    mongoengine.register_connection(alias='core', name='ujima_bilingual')
+
+    with open('config.json') as f:
+        config = json.load(f)
+
+    # Access API key using the config dictionary
+    mongo_db_url = config['mongo-db-url']
+
+    mongoengine.register_connection(alias='core', host=mongo_db_url)
