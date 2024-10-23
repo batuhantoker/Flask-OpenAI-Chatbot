17f5f0e51885ab0935bac3750bf0cadd9c7740eb
JayJhaveri1906 <jjhaveri1906@gmail.com>
2024-10-23 11:47:36 -0700

    Added additional userIds for testing

--- services/create_accounts_in_db.py
+++ services/create_accounts_in_db.py
@@ -1,6 +1,6 @@
 import services.data_service as svc
 
-user_ids = ['12345', '67890', '11122', '33344'] 
+user_ids = ['12345', '67890', '11122', '33344', '55678', '99001', '23456', '78901', '34567', '89012', '56789', '10123', '67812', '91234']
 
 def run_create_accounts():
     for user_id in user_ids:
